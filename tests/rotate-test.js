d44fc3c49e4b7b3f7aeeba3e1b1218de5b7ab009
rockbot <raquel@skookum.com>
2012-11-25 21:14:36 -0500

    Starts adding transformations

---
+++ tests/rotate-test.js
@@ -0,0 +1,9 @@
+var Vector = require('../').vector;
+var Rotate = require('../').rotate;
+var should = require('should');
+
+describe('Rotating a point', function () {
+  it('should rotate it around the z-axis', function (done) {
+    done();
+  });
+});
