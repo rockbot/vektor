ac917e943c6268031df32e12cc30c7ca9eec2855
rockbot <rockbot01@gmail.com>
2012-12-24 13:06:27 -0500

    adds a few rotation matrices with appropriate tests and fixes a vector bug

--- tests/rotate-test.js
+++ tests/rotate-test.js
@@ -2,8 +2,26 @@ var Vector = require('../').vector;
 var Rotate = require('../').rotate;
 var should = require('should');
 
-describe('Rotating a point', function () {
-  it('should rotate it around the z-axis', function (done) {
+describe('Rotating a point about the z-axis', function () {
+  it('should rotate it by pi/2 radians', function (done) {
+    var v = new Vector([1,0,0]);
+    var M = new Rotate.RotZ(Math.PI/2);
+    M.dot(v).v.should.eql([0,1,0]);
+    done();
+  });
+  it('should rotate it by 90 degrees', function (done) {
+    var v = new Vector([1,0,0]);
+    var M = new Rotate.RotZ(90, true);
+    M.dot(v).v.should.eql([0,1,0]);
+    done();
+  });
+});
+
+describe('Rotating a point about the y-axis', function () {
+  it('should rotate it by pi/2 radians', function (done) {
+    var v = new Vector([1,0,0]);
+    var M = new Rotate.RotY(Math.PI/2);
+    M.dot(v).v.should.eql([0, 0, -1]);
     done();
   });
 });
