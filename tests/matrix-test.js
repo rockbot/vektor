3199584935c258058a1446f14805db3ce66ca315
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-13 16:50:42 +0200

    improper use of done();

--- tests/matrix-test.js
+++ tests/matrix-test.js
@@ -3,37 +3,33 @@ var Vector = require('../').vector;
 var should = require('should');
 
 describe('Creating matrices: ', function () {
-  it('should create a Matrix', function (done) {
+  it('should create a Matrix', function () {
     var A = new Matrix(3,4);
     A.size.should.eql({rows: 3, cols: 4});
-    done();
   });
-  it('should create a square Matrix', function (done) {
+  it('should create a square Matrix', function () {
     var B = new Matrix(2);
     B.size.should.eql({rows: 2, cols: 2});
-    done();
   });
 
   describe('2x2 matrices: ', function () {
     var A = new Matrix(2,2);
     var B = new Matrix(2,2);
 
-    it('should set a specific value', function (done) {
+    it('should set a specific value', function () {
       // var A = new Matrix(3,4);
       A.set(0,0,1);
       A.set(0,1,2);
       A.set(1,0,3);
       A.set(1,1,4);
       A.m[1][1].should.eql(4);
-      done();
     });
 
-    it('should get the value at a specific location', function (done) {
+    it('should get the value at a specific location', function () {
       A.get(1,1).should.eql(4);
-      done();
     });
 
-    it('should add the two matrices together', function (done) {
+    it('should add the two matrices together', function () {
       B.set(0,0,2);
       B.set(0,1,4);
       B.set(1,0,6);
@@ -41,53 +37,47 @@ describe('Creating matrices: ', function () {
       var C = A.add(B);
       C.get(0,0).should.eql(3);
       C.get(1,0).should.eql(9);
-      done();
     });
 
-    it('should multiply a vector by a matrix', function (done) {
+    it('should multiply a vector by a matrix', function () {
       var v = new Vector(2,3);
       var C = A.dot(v);
       C.v.should.eql([8,18]);
-      done();
     });
 
-    it('should multiply two matrices together', function (done) {
+    it('should multiply two matrices together', function () {
       var C = A.dot(B);
       C.get(0,0).should.eql(14);
       C.get(0,1).should.eql(20);
       C.get(1,0).should.eql(30);
       C.get(1,1).should.eql(44);
-      done();
     });
 
-    it('should find the transpose of the matrix', function (done) {
+    it('should find the transpose of the matrix', function () {
       var C = A.transpose();
       C.get(0,0).should.eql(A.get(0,0));
       C.get(0,1).should.eql(A.get(1,0));
       C.get(1,1).should.eql(A.get(1,1));
       C.get(1,0).should.eql(A.get(0,1));
-      done();
     });
 
-    it('should calculate the determinant of the matrix', function (done) {
+    it('should calculate the determinant of the matrix', function () {
       var C = A.det();
       C.should.eql(-2);
-      done();
     });
 
-    it('should calculate the trace of the matrix', function (done) {
+    it('should calculate the trace of the matrix', function () {
       var C = A.trace();
       C.should.eql(5);
-      done();
     });
 
-    it('should scale the matrix by a value', function (done) {
+    it('should scale the matrix by a value', function () {
       var C = A.scale(-1);
       C.get(0,0).should.eql(-1);
       C.get(0,1).should.eql(-2);
       C.get(1,0).should.eql(-3);
       C.get(1,1).should.eql(-4);
-      done();
+
     });
   });
 
@@ -95,7 +85,7 @@ describe('Creating matrices: ', function () {
     var A = new Matrix(3,3);
     var B = new Matrix(3,3);
 
-    it('should set a specific value', function (done) {
+    it('should set a specific value', function () {
       A.set(0,0,1);
       A.set(0,1,2);
       A.set(0,2,3);
@@ -106,15 +96,14 @@ describe('Creating matrices: ', function () {
       A.set(2,1,8);
       A.set(2,2,9);
       A.m[1][0].should.eql(4);
-      done();
     });
 
-    it('should get the value at a specific location', function (done) {
+    it('should get the value at a specific location', function () {
       A.get(1,1).should.eql(5);
-      done();
+
     });
 
-    it('should add the two matrices together', function (done) {
+    it('should add the two matrices together', function () {
       B.set(0,0,2);
       B.set(0,1,4);
       B.set(0,2,6);
@@ -127,17 +116,15 @@ describe('Creating matrices: ', function () {
       var C = A.add(B);
       C.get(0,0).should.eql(3);
       C.get(1,0).should.eql(12);
-      done();
     });
 
-    it('should multiply a vector by a matrix', function (done) {
+    it('should multiply a vector by a matrix', function () {
       var v = new Vector(2,3,4);
       var C = A.dot(v);
       C.v.should.eql([20, 47, 74]);
-      done();
     });
 
-    it('should multiply two matrices together', function (done) {
+    it('should multiply two matrices together', function () {
       var C = A.dot(B);
       C.get(0,0).should.eql(60);
       C.get(0,1).should.eql(72);
@@ -148,32 +135,27 @@ describe('Creating matrices: ', function () {
       C.get(2,0).should.eql(204);
       C.get(2,1).should.eql(252);
       C.get(2,2).should.eql(300);
-      done();
     });
 
-    it('should find the transpose of the matrix', function (done) {
+    it('should find the transpose of the matrix', function () {
       var C = A.transpose();
       C.get(0,0).should.eql(A.get(0,0));
       C.get(0,1).should.eql(A.get(1,0));
       C.get(1,1).should.eql(A.get(1,1));
       C.get(1,0).should.eql(A.get(0,1));
-      done();
     });
 
-    it('should calculate the determinant of the matrix', function (done) {
+    it('should calculate the determinant of the matrix', function () {
       var C = A.det();
-      console.log(C);
       C.should.eql(0);
-      done();
     });
 
-    it('should calculate the trace of the matrix', function (done) {
+    it('should calculate the trace of the matrix', function () {
       var C = A.trace();
       C.should.eql(15);
-      done();
     });
 
-    it('should scale the matrix by a value', function (done) {
+    it('should scale the matrix by a value', function () {
       var C = A.scale(-1);
       C.get(0,0).should.eql(-1);
       C.get(0,1).should.eql(-2);
@@ -184,7 +166,6 @@ describe('Creating matrices: ', function () {
       C.get(2,0).should.eql(-7);
       C.get(2,1).should.eql(-8);
       C.get(2,2).should.eql(-9);
-      done();
     });
   });
 });
